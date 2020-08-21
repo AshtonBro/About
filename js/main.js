05d531ce231e904e91f7594b9b531df28ebfd449
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-08-21 23:14:19 +0500

    swipper done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 new WOW().init();
 
 var mySwiper = new Swiper('.swiper-container', {
-    speed: 400,
-    spaceBetween: 100
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active'
+    },
 });
